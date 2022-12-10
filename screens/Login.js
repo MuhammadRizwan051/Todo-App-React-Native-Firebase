a66c0994a2868ca3118b995970c350febe402369
MuhammadRizwan051 <memonrizwan051@gmail.com>
2022-12-10 23:30:22 +0500

    Login UI, Button and TextField Component, Global Style

--- screens/Login.js
+++ screens/Login.js
@@ -1,15 +1,39 @@
-import React from 'react'
-import { View, Text } from 'react-native'
-
-
-function Login() {
+import React from 'react';
+import { View, Text, TouchableOpacity } from 'react-native';
+import SMButton from '../component/SMButton';
+import SMTextField from '../component/SMTextInput';
+// import auth from '@react-native-firebase/auth';
+import styles from '../styling';
+function Login({ navigation }) {
   return (
     <>
-      <View>
-        <Text>Login</Text>
+      <View style={[styles.bgLight, styles.h100, styles.flexCenter]}>
+        <Text style={[styles.textPrimary, styles.fs2]}>Login</Text>
+        <View style={[styles.p2, styles.w100]}>
+          <SMTextField label="Email" />
+        </View>
+        <View style={[styles.p2, styles.w100]}>
+          <SMTextField label="Password" />
+        </View>
+        <View style={[styles.p2, styles.w100]}>
+          <SMButton label="Login" />
+        </View>
+        <View style={[styles.p2, styles.w100]}>
+          <Text style={[styles.textCenter, styles.fs4, styles.flexCenter]}>
+            are You new user?{' '}
+            <TouchableOpacity
+              onPress={() => {
+                navigation.navigate('SignUp');
+              }}
+              style={styles.flexCenter}>
+              <Text style={[styles.textCenter, styles.fs3, styles.textPrimary]}>
+                Register
+              </Text>
+            </TouchableOpacity>
+          </Text>
+        </View>
       </View>
     </>
-  )
+  );
 }
-
-export default Login
+export default Login;
