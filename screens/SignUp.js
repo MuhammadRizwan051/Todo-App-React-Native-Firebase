dbcb865d7c4c03e1f172201cb1b27923f87e1337
MuhammadRizwan051 <memonrizwan051@gmail.com>
2022-12-10 23:33:03 +0500

    SignUp UI

--- screens/SignUp.js
+++ screens/SignUp.js
@@ -1,15 +1,41 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import React, {useState} from 'react';
+import {View, Text, TouchableOpacity} from 'react-native';
+// import auth from '@react-native-firebase/auth';
+import styles from '../styling';
+import SMTextField from '../component/SMTextInput';
+import SMButton from '../component/SMButton';
+// import database from '@react-native-firebase/database';
 
+function SignUp({navigation}) {
+  const [model, setModel] = useState({});
 
-function SignUp() {
   return (
     <>
-      <View>
+      <View style={[styles.h100, styles.flexCenter, styles.bgLight]}>
         <Text>SignUp</Text>
+        <View style={[styles.p2, styles.w100]}>
+          <SMTextField
+            onChangeText={e => setModel({...model, userName: e})}
+            label="User Name"
+          />
+        </View>
+        <View style={[styles.p2, styles.w100]}>
+          <SMTextField
+            onChangeText={e => setModel({...model, email: e})}
+            label="Email"
+          />
+        </View>
+        <View style={[styles.p2, styles.w100]}>
+          <SMTextField
+            onChangeText={e => setModel({...model, password: e})}
+            label="Password"
+          />
+        </View>
+        <View style={[styles.p2, styles.w100]}>
+          <SMButton label="Sign Up" />
+        </View>
       </View>
     </>
-  )
+  );
 }
-
-export default SignUp
+export default SignUp;
